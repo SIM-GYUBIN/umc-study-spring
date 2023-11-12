771c9630589143a162ee71220f3564c4cc9d8d59
Sim-GyuBin <skb0606@gmail.com>
2023-11-12 23:22:32 +0900

    Initial commit

---
+++ src/main/java/umc/study/domain/mapping/MemberMission.java
@@ -0,0 +1,35 @@
+package umc.study.domain.mapping;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc.study.domain.FoodCategory;
+import umc.study.domain.Member;
+import umc.study.domain.Mission;
+import umc.study.domain.common.BaseEntity;
+import umc.study.domain.enums.MissionStatus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class MemberMission extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private MissionStatus status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mission_id")
+    private Mission mission;
+}
